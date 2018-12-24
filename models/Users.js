872b6dd001c801d9dfad6d71ef1100044a3c6b13
Tommy <carrascalx@gmail.com>
2018-12-23 23:50:31 -0500

    organized folders and fixed jwt response

--- models/Users.js
+++ models/Users.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 
 const {Schema} = mongoose;
 
+//User schema
 const UsersSchema = new Schema({
     email: String,
     password: String,
+    username: String,
+    profile: {photos: [],bio: String,skills: []},
+    matches: [],
+    hackathon: String,
+    team: String
 });
 
 module.exports = mongoose.model('Users', UsersSchema);
--- services/user/index.js
+++ services/user/index.js
@@ -1,23 +1,37 @@
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/Users');
+const User = require('../../models/Users');
 
-const signUp = async (email,password) => {
+//create user
+const signUp = async (email,password,username) => {
     try{
-        const user = await User.findOne({
+        let user = await User.findOne({
             email:email
-        })
+        });
 
         if(user)
-            return('user already exists')
+            return null
 
-        const hashedPass = bcrypt.hashSync(password)
+        user = await User.findOne({
+                usernmae:usernmae
+        });
+
+        if(user)
+            return null;
+
+        const hashedPass = bcrypt.hashSync(password);
         
         const newUser = await User.create({
             email,
-            password: hashedPass    
-        })
+            password: hashedPass,
+            username,
+            profile:{photos:[],bio: null,skills:[]},
+            matches:[],
+            hackathon: null,
+            team: null
+
+        });
 
         await newUser.save();
 
@@ -31,35 +45,38 @@ const signUp = async (email,password) => {
         }, 'secret');
 
     } catch(err){
-        return 'Invalid information';
+        return null;
     }
         
    
 }
-
+//Authenticate user
 const login = async (email,password) => {
     try{
         const user = await User.findOne({
             email
-        })
+        });
 
         const correctPass = bcrypt.compareSync(password,user.password);
 
         if(!user || !correctPass)
-            return('wrong login info')
+            return null;
         
         const today = new Date();
         const expirationDate = new Date(today);
         expirationDate.setDate(today.getDate() + 60);
         
+        let id = user.id;
+
         return jwt.sign({
-            email,
+            id,
             exp: parseInt(expirationDate.getTime() / 1000, 10),
         }, 'secret');
 
     } catch(err){
-        return 'Invalid information'
+        return null;
     }
 }
 
-module.exports =  {signUp,login}
+
+module.exports =  {signUp,login};
