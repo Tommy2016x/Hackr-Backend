a84a423576d38e109a3fd3b92c2edc8423ba5e0b
Tommy <carrascalx@gmail.com>
2018-12-29 01:39:27 -0500

    added bodyparser urlencoded

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const users = require('./routes/api/users')
 const locationService = require('./routes/api/geolocation');
 
 const app = express();
+
 app.use(bodyparser.urlencoded({
     extended: true
 }));
 app.use(bodyparser.json());
+
 app.use('/users',users);
 app.use('/location',locationService);
-
-
 
 app.listen(3000);
 
